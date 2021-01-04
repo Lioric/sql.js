ac8b7166c969a3df1c791f2a4f769b1ae02a829d
Lioric <andres@ofusiontechnologies.com>
2021-01-04 17:38:28 -0500

    Add test http

--- examples/start_local_server.py
+++ examples/start_local_server.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-import BaseHTTPServer, SimpleHTTPServer, os
+import BaseHTTPServer, GZipSimpleHTTPServer, os
  
 # We need to host from the root because we are going to be requesting files inside of dist
 os.chdir('../')
 port=8081
 print "Running on port %d" % port
  
-SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm'] = 'application/wasm'
+GZipSimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm'] = 'application/wasm'
 
-httpd = BaseHTTPServer.HTTPServer(('localhost', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd = BaseHTTPServer.HTTPServer(('localhost', port), GZipSimpleHTTPServer.SimpleHTTPRequestHandler)
  
-print "Mapping \".wasm\" to \"%s\"" % SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm']
+print "Mapping \".wasm\" to \"%s\"" % GZipSimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm']
 httpd.serve_forever()
