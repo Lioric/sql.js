1185216aae9273dc839ca61be37e7cb7b2ebc864
Lioric <andres@ofusiontechnologies.com>
2021-08-23 10:36:56 -0500

    Implemented multiple databases and db commands

--- src/worker.js
+++ src/worker.js
@@ -4,80 +4,169 @@
 
 "use strict";
 
-var db;
+var _dbList = {};
 
 function onModuleReady(SQL) {
-    function createDb(data) {
-        if (db != null) db.close();
-        db = new SQL.Database(data);
-        return db;
+    function getDB(name, data) {
+        if (_dbList[name] != null) return _dbList[name];
+        try {
+        	_dbList[name] = new SQL.Database(name, data);
+			return _dbList[name];
+		}
+		catch(err) {
+			return err;
+		}
+
     }
 
     var buff; var data; var result;
-    data = this["data"];
-    switch (data && data["action"]) {
-        case "open":
-            buff = data["buffer"];
-            createDb(buff && new Uint8Array(buff));
-            return postMessage({
-                id: data["id"],
-                ready: true
-            });
-        case "exec":
-            if (db === null) {
-                createDb();
-            }
-            if (!data["sql"]) {
-                throw "exec: Missing query string";
-            }
-            return postMessage({
-                id: data["id"],
-                results: db.exec(data["sql"], data["params"])
-            });
-        case "each":
-            if (db === null) {
-                createDb();
-            }
-            var callback = function callback(row) {
-                return postMessage({
-                    id: data["id"],
-                    row: row,
-                    finished: false
-                });
-            };
-            var done = function done() {
-                return postMessage({
-                    id: data["id"],
-                    finished: true
-                });
-            };
-            return db.each(data["sql"], data["params"], callback, done);
-        case "export":
-            buff = db["export"]();
-            result = {
-                id: data["id"],
-                buffer: buff
-            };
-            try {
-                return postMessage(result, [result]);
-            } catch (error) {
-                return postMessage(result);
-            }
-        case "close":
-            if (db) {
-                db.close();
-            }
-            return postMessage({
-                id: data["id"]
-            });
-        default:
-            throw new Error("Invalid action : " + (data && data["action"]));
+    data = this.data;
+
+	var cmds = data.commands;
+	var size = cmds.length;
+
+	for(var i=0; i<size; ++i) {
+		var cmd = cmds[i];
+
+		if(!cmd.name) {
+			throw "command: Missing name string";
+		}
+
+		var db = null;
+		var res = {};
+
+    	switch(cmd.action) {
+        	case "open":
+				if(_dbList[cmd.name]) {
+					_dbList[cmd.name].close();
+					_dbList[cmd.name] = null;
+				}
+
+            	buff = cmd.buffer;
+            	db = getDB(cmd.name, buff && new Uint8Array(buff));
+
+            	postMessage({
+                	id: cmd.id,
+                	index: cmd.index,
+//					version: cmd.version,
+					buffer: cmd.buffer,
+					extra: cmd.extra,
+					error: typeof db === "string" ? db : null
+            	}, [cmd.buffer]);
+
+            	break;
+
+        	case "exec":
+        		if(!cmd.sql) {
+	                throw "exec: Missing query string";
+    	        }
+
+        		db = getDB(cmd.name);
+
+				res = {
+                	id: cmd.id,
+	                index: cmd.index,
+					extra: cmd.extra
+        	    };
+
+				try {
+					res.results = db.exec(cmd.sql, cmd.params);
+					postMessage(res);
+				}
+				catch(err) {
+					postMessage({error: err, commands: [res]});
+				}
+
+//            	postMessage(res);
+
+            	break;
+
+	        case "each":
+    	        db = getDB(cmd.name);
+
+        	    var callback = function(row) {
+            	    return postMessage({
+                	    id: cmd.id,
+                    	index: cmd.index,
+						extra: cmd.extra,
+	                    row: row,
+    	                finished: false
+        	        });
+	            };
+    	        var done = function() {
+        	        return postMessage({
+            	        id: data.id,
+                	    index: cmd.index,
+						extra: cmd.extra,
+                    	finished: true
+	                });
+    	        };
+
+	            db.each(cmd.sql, cmd.params, callback, done);
+
+    	        break;
+
+	        case "export":
+    	    	db = getDB(cmd.name);
+        	    buff = db.export();
+            	result = {
+	                id: cmd.id,
+    	            index: cmd.index,
+					version: cmd.version,
+					extra: cmd.extra,
+        	        buffer: buff.buffer || buff
+	            };
+    	        try {
+        	        postMessage(result, [buff.buffer || buff]);
+            	} catch (error) {
+	                postMessage(result);
+    	        }
+
+        	    break;
+
+        	case "close":
+        		if(_dbList[cmd.name]) {
+					_dbList[cmd.name].close();
+					_dbList[cmd.name] = null;
+				}
+
+            	postMessage({
+                	id: cmd.id,
+					extra: cmd.extra,
+                	index: cmd.index
+            	});
+
+            	break;
+
+			case "searchfilter":
+				var filter;
+				var msg;
+				db = getDB(cmd.name);
+				if(db) {
+					try {
+						filter = SQL.FullTextSearch.createSearchFilter("en", cmd.text);
+					}
+					catch(err) {
+						msg = err;
+					}
+				}
+				postMessage({id: "sync", results: filter.slice(), index: cmd.index, extra: cmd.extra, error: msg})
+
+				break;
+
+	        default:
+    	        throw new Error("Invalid action : " + cmd.action);
+    	}
+
     }
 }
 
 function onError(err) {
     return postMessage({
-        id: this["data"]["id"],
+    	commands: this.data.commands,
+//        id: this["data"]["id"],
+//        index: this["data"]["index"],
+//        extra: this.data.extra,
         error: err["message"]
     });
 }
@@ -90,4 +179,6 @@ if (typeof importScripts === "function") {
             .then(onModuleReady.bind(event))
             .catch(onError.bind(event));
     };
+
+//	self.filter = function() { console.log("filter")};
 }
